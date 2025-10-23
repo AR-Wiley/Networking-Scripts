9552cf010847618b5934a5643108079e2257689e
AR-Wiley <ar.wiley805@outlook.com>
2025-10-22 20:07:53 -0500

    first commit

---
+++ Ping-Test.ps1
@@ -0,0 +1,22 @@
+﻿function Ping-Test {
+
+    $BaseIP = "192.168.1."
+    $range = 1..10
+    $machines = @()
+
+    forEach ($i in $range){
+
+        $ip = "$baseIP$i"
+
+        if(Test-Connection -ComputerName $ip -Count 1 -Quiet -ErrorAction SilentlyContinue) {
+            $machines += $ip
+        }
+
+    }
+
+    Write-Output "Machines that are currently online: "
+    return $machines
+
+}
+
+Ping-Test
---
+++ Port-Scan.ps1
@@ -0,0 +1,26 @@
+﻿function Port-Scan {
+
+    $BaseIP = "192.168.1."
+    $range = 1..3
+    $ports = @(21,22,23,80,139,3389,8080)
+    $machines = @()
+
+    forEach ($i in $range) {
+        $ip = "$BaseIP$i"
+
+        forEach ($j in $ports){
+
+            $port_scan = Test-NetConnection $ip -Port $j
+
+            if($port_scan.TcpTestSucceeded -eq $true){
+
+                $machines += $port_scan
+
+            }
+        }
+    }
+
+    return $machines
+
+}
+
